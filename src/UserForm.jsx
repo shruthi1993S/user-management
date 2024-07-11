87e85736f10d42555e43c7bb59a74a3958c334c7
shruthi1993s <shruthiji06@gmail.com>
2024-07-11 21:31:29 +0530

    updated

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -1,55 +1,70 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material'
-
+import React, { useEffect, useState } from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
 
 function UserForm({ user, onSave, onCancel }) {
-    const [formData, setFormData] = useState({ name: '', email: '' });
+  const [formData, setFormData] = useState({ name: '', email: '' });
+  const [errors, setErrors] = useState({});
 
-    useEffect(() => {
-        if (user) {
-            setFormData(user);
-        }
-    }, [user]);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value })
+  useEffect(() => {
+    if (user) {
+      setFormData(user);
     }
+  }, [user]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSave(formData);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const validate = () => {
+    let tempErrors = {};
+    if (!formData.name) tempErrors.name = "Name is required.";
+    if (!formData.email) tempErrors.email = "Email is required.";
+    setErrors(tempErrors);
+    return Object.keys(tempErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      onSave(formData);
     }
+  };
 
-    return (
-        <Dialog open onClose={onCancel}>
-            <DialogTitle>{user.id ? 'Edit User' : 'Add User'}</DialogTitle>
-            <DialogContent>
-                <TextField
-                    autoFocus
-                    margin='dense'
-                    name='name'
-                    label='Name'
-                    type='text'
-                    fullWidth
-                    value={formData.name}
-                    onChange={handleChange}></TextField>
-                <TextField
-                    autoFocus
-                    margin='dense'
-                    name='email'
-                    label='Email'
-                    type='email'
-                    fullWidth
-                    value={formData.email}
-                    onChange={handleChange}></TextField>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={onCancel} color='primary'>Cancel</Button>
-                <Button onClick={handleSubmit} color='primary'>Save</Button>
-            </DialogActions>
-        </Dialog>
-    )
+  return (
+    <Dialog open onClose={onCancel}>
+      <DialogTitle>{user._id ? 'Edit User' : 'Add User'}</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin='dense'
+          name='name'
+          label='Name'
+          type='text'
+          fullWidth
+          value={formData.name}
+          onChange={handleChange}
+          error={!!errors.name}
+          helperText={errors.name}
+        />
+        <TextField
+          margin='dense'
+          name='email'
+          label='Email'
+          type='email'
+          fullWidth
+          value={formData.email}
+          onChange={handleChange}
+          error={!!errors.email}
+          helperText={errors.email}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel} color='primary'>Cancel</Button>
+        <Button onClick={handleSubmit} color='primary'>Save</Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
-export default UserForm
+export default UserForm;
